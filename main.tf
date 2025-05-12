df41e2c2357c09bd8d44e14006833773f5ef52d7
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-05-12 15:02:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "sam-458313"        # ← your GCP project ID
+  project = "saicharan-457910"        # ← your GCP project ID
   region  = "us-west1"          # optional, for regional resources
   zone    = "us-west1-a"        # for zonal resources like compute instances
 }
